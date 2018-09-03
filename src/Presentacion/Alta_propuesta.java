8cf33ea8dce39cbbcee187b089799f436b33c8c6
Luchizina <cinthiaramburu@gmail.com>
2018-09-03 17:35:14 -0300

    Merge origin/master
    Conflicts:
    	src/Presentacion/Alta_propuesta.java

--- src/Presentacion/Alta_propuesta.java
+++ src/Presentacion/Alta_propuesta.java
@@ -555,9 +555,11 @@ public static String getHoraActual() {
          {
              String catego= jTree1.getSelectionPath().getLastPathComponent().toString();
              if(this.IP.existeTitulo(jTitulo.getText())==true){
-                 javax.swing.JOptionPane.showMessageDialog(null, "Ya existe esta propuesta");
+                    JOptionPane.showMessageDialog(null, "El titulo ya esta en uso");
+           
              
-             
+             }else {
+           
              
         boolean ok=IP.AgregarPropuesta(jTitulo.getText(), jDesc.getText(), jDate.getDate(), Integer.parseInt(jPrecioE.getText()),0,jDate.getDate(),TRetorno,Integer.parseInt(jPrecioT.getText()),catego,estA,urlimagen.getText(),parte4,hora,jLugar.getText());
              if (ok){
@@ -566,9 +568,7 @@ public static String getHoraActual() {
              }else{
             javax.swing.JOptionPane.showMessageDialog(null,"Error al dar de alta la propuesta o la propuesta ya existe");
              }
-         }else {
-                 JOptionPane.showMessageDialog(null, "El titulo ya esta en uso");
-             }
+           }
              } 
          }
         
