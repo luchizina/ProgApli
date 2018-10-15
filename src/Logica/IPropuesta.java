b5bbaa2a19bc90101fadba6af3f272f98427be89
Lucia <luciaurrutty@gmail.com>
2018-10-15 16:56:22 -0300

    Merge origin/master
    Conflicts:
    	src/Logica/ctrlPropuesta.java

--- src/Logica/IPropuesta.java
+++ src/Logica/IPropuesta.java
@@ -25,6 +25,7 @@ import javax.swing.tree.DefaultTreeModel;
  * @author Luchi
  */
 public interface IPropuesta {
+    public List<DtComentarios> traerComentarios(String prop);
     public void extender(String tit) throws SQLException ;
     public List<DtPropuesta> listaTDL(String txt);
     public boolean yaFavoriteo(Usuario u,String p);
